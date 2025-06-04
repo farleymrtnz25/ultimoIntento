3363f20b3078b2d7a84d97509bf3603e04a23aa8
farleymrtnz25 <frlmrtnz@gmail.com>
2025-06-03 20:37:28 -0500

    Descripción de los cambios realizados

--- netlify/funciones/usuarios.js
+++ netlify/funciones/usuarios.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require("cors");
 var serverless = require('serverless-http');
 var app = express();
+require('dotenv').config(); // Cargar variables de entorno desde .env
 var usuroutes = require("../../backend/routes/usuariosrutas.js");
 
 app.use(cors());
@@ -9,6 +10,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/.netlify/functions', usuroutes);
+app.use('/.netlify/functiones', usuroutes);
 
 exports.handler = serverless(app);
